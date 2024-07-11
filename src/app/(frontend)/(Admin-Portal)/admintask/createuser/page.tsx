fec7c37a3685b3fa358e7edbd5ec518985ad090d
mayankjonwal02 <jonwal.1@iitj.ac.in>
2024-07-12 03:12:27 +0530

    dim

--- src/app/(frontend)/(Admin-Portal)/admintask/createuser/page.tsx
+++ src/app/(frontend)/(Admin-Portal)/admintask/createuser/page.tsx
@@ -1,14 +1,12 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
 import { Input } from "@/components/ui/input"
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -20,98 +18,77 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import cities from '@/constants/ConstantData'
 
-
-
 const CreateUser = () => {
     const router = useRouter();
-    // useEffect(() => {
-    //     const login = localStorage.getItem("login_admin");
-    
-    //     if (login === "true") {
-    //         router.push('/admin')
-    //     }
-    
-    // }, []);
     const { toast } = useToast()
     const [passwordShown, setPasswordShown] = useState(false);
     const [passwordShown1, setPasswordShown1] = useState(false);
     const [admin, setAdmin] = useState("");
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [city , setCity] = useState('');
-    const [citycode , setCitycode] = useState('');
-    const [loading, setloading] = useState(false)
+    const [city, setCity] = useState('');
+    const [citycode, setCitycode] = useState('');
+    const [loading, setloading] = useState(false);
+
     const handleregister = () => {
         if (password === confirmPassword) {
-            
-            if(admin !== '' && password !== '' && city !== '' && citycode !== '')
-                {
-                    setloading(true)
-                    fetch('/api/createuser', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            unique_id: admin,
-                            password: password,
-                            city: city,
-                            citycode: citycode
-                        }),
-                    })
-                        .then((response) => response.json())
-                        .then((data) => {
-                            setloading(false)
-                            if(data.executed)
-                            {
-                                toast({
-                                    title: data.message,
-                                    
-                                    variant: 'success',
-                                })
-                                
-                            }
-                            else
-                            {
-                                toast({
-                                    title: data.message,
-                                    variant: 'destructive',
-                                    description: data.error
-                                })
-                            }
+            if(admin !== '' && password !== '' && city !== '' && citycode !== '') {
+                setloading(true)
+                fetch('/api/createuser', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        unique_id: admin,
+                        password: password,
+                        city: city,
+                        citycode: citycode
+                    }),
+                })
+                .then((response) => response.json())
+                .then((data) => {
+                    setloading(false)
+                    if(data.executed) {
+                        toast({
+                            title: data.message,
+                            variant: 'success',
                         })
-                        .catch((error) => {
-                            setloading(false)
-                            console.error('Error:', error);
-                            toast({
-                                title: "Error",
-                                variant: 'destructive',
-                                description: error
-                            })
-                        });
-                }
-            else{
+                    } else {
+                        toast({
+                            title: data.message,
+                            variant: 'destructive',
+                            description: data.error
+                        })
+                    }
+                })
+                .catch((error) => {
+                    setloading(false)
+                    console.error('Error:', error);
+                    toast({
+                        title: "Error",
+                        variant: 'destructive',
+                        description: error
+                    })
+                });
+            } else {
                 toast({
                     title: 'Please fill all the fields',
                     variant: 'destructive',
-            
                 })
             }
-
-        }
-        else {
+        } else {
             toast({
                 title: 'Password Mismatch',
                 variant: 'destructive',
             })
         }
     };
+
     const togglePasswordVisibility = () => {
         setPasswordShown(!passwordShown);
     };
@@ -119,52 +96,47 @@ const CreateUser = () => {
     const toggleConfirmPasswordVisibility = () => {
         setPasswordShown1(!passwordShown1);
     };
+
     return (
         <div className='flex flex-col w-full h-screen items-center justify-center'>
-
-            <div className=' w-full text-center text-5xl text-green-5 font-extrabold justify-center items-center mb-10'>
-
+            <div className='w-full text-center text-5xl text-green-5 font-extrabold justify-center items-center mb-10'>
                 Register New User
             </div>
-            <Card className=' drop-shadow-lg relative backdrop-blur-xl bg-white/30 w-4/5 md:w-2/5 lg:w-5/12  py-10 mt-[90px]  border-4 border-green-5 '>
-                <div className='absolute flex flex-row justify-center items-center w-full top-[-100px] '>
-                    <FontAwesomeIcon icon={faUser} className='text-green-5 w-[120px] h-[120px] bg-green-2 rounded-full p-6 border border-4 border-green-5' size="3x" />
-
+            <Card className='drop-shadow-lg relative backdrop-blur-xl bg-white/30 w-11/12 md:w-2/5 lg:w-4/12 pt-10 mt-16 border-4 border-green-5'>
+                <div className='absolute flex justify-center w-full top-[-70px]'>
+                    <FontAwesomeIcon icon={faUser} className='text-green-5 w-[90px] h-[90px] bg-green-2 rounded-full p-6 border border-4 border-green-5' size="3x" />
                 </div>
-                <CardHeader>
-
-                </CardHeader>
-                <CardContent className=' mt-[50px] flex flex-col justify-start items-start'>
-
-
+                <CardHeader />
+                <CardContent className='mt-[10px] flex flex-col justify-start items-start '>
                     <Label htmlFor="email">User ID</Label>
-                    <Input className='mb-6'
+                    <Input
+                        className='mb-6'
                         type="text"
                         placeholder="Admin ID"
                         value={admin}
-                        onChange={(e) => setAdmin(e.target.value)} />
-
+                        onChange={(e) => setAdmin(e.target.value)}
+                    />
                     <div className='flex flex-row justify-start items-center w-full mb-6'>
-
-                    <DropdownMenu >
-                        <DropdownMenuTrigger><Button className='bg-green-5 text-white hover:bg-green-4 hover:text-green-5   text-sm' variant="outline" >Choose City</Button></DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            
-                            {   cities.map((city) => (
-                                <DropdownMenuItem key={city.city} onClick={() => {setCity(city.city);setCitycode(city.city_id)}}>{city.city}</DropdownMenuItem>
-                            ))}
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                    <div className='flex grow font-bold text-green-5  text-center justify-center'>
-                            {city!==""?city:"No City Chosen"}
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Button className='bg-green-5 text-white hover:bg-green-4 hover:text-green-5 text-sm' variant="outline">
+                                    Choose City
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                {cities.map((city) => (
+                                    <DropdownMenuItem key={city.city} onClick={() => {setCity(city.city); setCitycode(city.city_id)}}>
+                                        {city.city}
+                                    </DropdownMenuItem>
+                                ))}
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                        <div className='flex grow font-bold text-green-5 text-center justify-center'>
+                            {city !== "" ? city : "No City Chosen"}
+                        </div>
                     </div>
-                    </div>
-
-
-
                     <Label htmlFor="password">Password</Label>
-
-                    <div className='relative mb-4 w-full' style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className='relative mb-4 w-full flex items-center'>
                         <Input
                             className='pr-10'
                             type={passwordShown ? 'text' : 'password'}
@@ -173,18 +145,15 @@ const CreateUser = () => {
                             onChange={(e) => setPassword(e.target.value)}
                         />
                         <button
-                            className='absolute inset-y-0 right-0  h-full px-4'
+                            className='absolute inset-y-0 right-0 h-full px-4'
                             onClick={togglePasswordVisibility}
                             style={{ cursor: 'pointer' }}
                         >
                             {passwordShown ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
                         </button>
                     </div>
-
-
                     <Label htmlFor="password mt-7">Confirm Password</Label>
-
-                    <div className='relative w-full' style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className='relative w-full flex items-center'>
                         <Input
                             className='pr-10'
                             type={passwordShown1 ? 'text' : 'password'}
@@ -193,7 +162,7 @@ const CreateUser = () => {
                             onChange={(e) => setConfirmPassword(e.target.value)}
                         />
                         <button
-                            className='absolute inset-y-0 right-0  h-full px-4'
+                            className='absolute inset-y-0 right-0 h-full px-4'
                             onClick={toggleConfirmPasswordVisibility}
                             style={{ cursor: 'pointer' }}
                         >
@@ -201,21 +170,15 @@ const CreateUser = () => {
                         </button>
                     </div>
                     <div className='flex flex-col justify-center items-center mt-6 w-full'>
-
-                        <Button className='bg-green-5 text-white hover:bg-green-4 hover:text-green-5 mt-7 px-9 py-5 text-lg' variant="outline" onClick={handleregister}>Submit 
-                        {loading?<div className="animate-spin w-[25px] h-[25px] border-4 border-t-4 border-white border-t-transparent  rounded-full ms-3"></div>
-:<></>}
-                        {/* <div className="animate-spin w-[25px] h-[25px] border-4 border-t-4 border-white border-t-transparent  rounded-full ms-3"></div> */}
-
+                        <Button className='bg-green-5 text-white hover:bg-green-4 hover:text-green-5 mt-7 px-9 py-3 text-lg' variant="outline" onClick={handleregister}>
+                            Submit
+                            {loading && <div className="animate-spin w-[25px] h-[25px] border-4 border-t-4 border-white border-t-transparent rounded-full ms-3"></div>}
                         </Button>
-                        </div>
-
+                    </div>
                 </CardContent>
-                <CardFooter>
-                </CardFooter>
+                {/* <CardFooter /> */}
             </Card>
         </div>
-
     )
 }
 
--- src/app/(frontend)/(Patient-Form)/page.tsx
+++ src/app/(frontend)/(Patient-Form)/page.tsx
@@ -5,20 +5,15 @@ import { Input } from "@/components/ui/input"
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
 import mylogo from '@/assets/aims_jodhpur2.png' 
-
-
-
 
 const DoctorLogin = () => {
     const router = useRouter();
@@ -74,54 +69,40 @@ const DoctorLogin = () => {
                     variant: 'destructive',
                 })
             }
-            
-            
-
         } else {
             console.log('Login Failed')
             toast({
                 title: 'Missing Credentials',
-               
                 variant: 'destructive',
             })
-
-
         }
     };
+    
     const togglePasswordVisibility = () => {
         setPasswordShown(!passwordShown);
     };
+
     return (
-        <div className='flex flex-col justify-top items-center h-screen'>
-
-            <div className='text-green-5 text-5xl md:text-7xl font-extrabold my-8'>SurVic Trials</div>
-            <div className='text-black text-4xl md:text-5xl font-extrabold my-3 '>Login</div>
-            <div className='flex flex-row justify-center items-center w-screen grow gap-[200px]'>
-                {/* <div className=' w-3/12 text-center hidden md:flex justify-center items-center '>
-                <FontAwesomeIcon icon={faUser} className='text-green-5 w-[270px] h-[270px]' size="3x" />
-                <img src={mylogo.src} alt="Description of my image" />               
-                </div> */}
-                <Card className=' drop-shadow-lg relative backdrop-blur-xl bg-white/30 w-4/5 md:w-2/5 lg:w-4/12  py-10 mt-[90px]  border-4 border-green-5 '>
-                    <div className='absolute flex flex-row justify-center items-center w-full top-[-70px] '>
-
-                        <img src={mylogo.src} alt="Description of my image" className='w-[150px] h-[150px] border-4 rounded-full border-green-5 ' /> </div>
-                    <CardHeader>
-
-                    </CardHeader>
-                    <CardContent className=' mt-[50px]'>
-
-
+        <div className='flex flex-col justify-start items-center h-screen w-screen overflow-x-hidden '>
+            <div className='text-green-5 text-5xl md:text-5xl lg:text-6xl font-extrabold mt-8'>SurVic Trials</div>
+            <div className='text-black text-4xl md:text-5xl font-extrabold my-7'>Login</div>
+            <div className='flex flex-col justify-center items-center w-full grow'>
+                <Card className='drop-shadow-lg relative backdrop-blur-xl bg-white/30 w-11/12 md:w-2/5 lg:w-3/12 pt-10  mt-16 border-4 border-green-5'>
+                    <div className='absolute flex justify-center w-full top-[-70px]'>
+                        <img src={mylogo.src} alt="Description of my image" className='w-[130px] h-[130px] border-4 rounded-full border-green-5' />
+                    </div>
+                    <CardHeader />
+                    <CardContent className='mt-[10px] '>
                         <Label htmlFor="email">Unique ID</Label>
-                        <Input className='mb-6'
+                        <Input
+                            className='mb-6'
                             type="text"
                             placeholder="Unique ID"
                             value={admin}
-                            onChange={(e) => setAdmin(e.target.value)} />
-
-
+                            onChange={(e) => setAdmin(e.target.value)}
+                        />
                         <Label htmlFor="password">Password</Label>
-
-                        <div className='relative' style={{ display: 'flex', alignItems: 'center' }}>
+                        <div className='relative flex items-center '>
                             <Input
                                 className='pr-10'
                                 type={passwordShown ? 'text' : 'password'}
@@ -130,28 +111,23 @@ const DoctorLogin = () => {
                                 onChange={(e) => setPassword(e.target.value)}
                             />
                             <button
-                                className='absolute inset-y-0 right-0  h-full px-4'
+                                className='absolute inset-y-0 right-0 h-full px-4'
                                 onClick={togglePasswordVisibility}
                                 style={{ cursor: 'pointer' }}
                             >
                                 {passwordShown ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
                             </button>
                         </div>
-                        <div className='flex flex-row justify-center items-center mt-6'>
-
-                            <Button className='bg-green-5 text-white hover:bg-green-4 hover:text-green-5 mt-7 px-9 py-5 text-lg' variant="outline" onClick={handleLogin}>Submit
-
-                            {loading && <div className='animate-spin h-[25px] w-[25px] border-4 border-t-4 border-green-3 border-t-transparent rounded-full ms-3'></div>}
-                            {/* <div className='animate-spin h-[25px] w-[25px] border-4 border-t-4 border-green-3 border-t-transparent rounded-full ms-3'></div> */}
+                        <div className='flex justify-center mt-6'>
+                            <Button className='bg-green-5 text-white hover:bg-green-4 hover:text-green-5 mt-7 px-9 py-3 text-lg' variant="outline" onClick={handleLogin}>
+                                Submit
+                                {loading && <div className='animate-spin h-[25px] w-[25px] border-4 border-t-4 border-green-3 border-t-transparent rounded-full ms-3'></div>}
                             </Button>
                         </div>
-
                     </CardContent>
-                    <CardFooter>
-                    </CardFooter>
+                    <CardFooter />
                 </Card>
             </div>
-
         </div>
     )
 }
